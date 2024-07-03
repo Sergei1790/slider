92a376e7d43478c7f41a98d470e6c0c70f89cc19
Sergei <40200071+Sergei1790@users.noreply.github.com>
2024-07-03 11:29:43 +0300

    new

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function initializeSlider(){
 function showSlide(index){
     if(index >= slides.length){
         slideIndex = 0;
-    } else if(index <= 0){
+    } else if(index < 0){
         slideIndex = slides.length - 1;
     }
     slides.forEach(slide =>{
